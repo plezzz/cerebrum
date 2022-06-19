3bdbadc612f1b79a5d9fc1db9f173498734b4967
Niki <nikid12e@gmail.com>
2022-06-20 00:22:10 +0300

    Test search

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient\Patient;
 use App\Service\Patient\PatientServiceInterface;
+use SlopeIt\BreadcrumbBundle\Annotation\Breadcrumb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,17 +20,18 @@ class SearchController extends AbstractController
         $this->patientService = $patientService;
     }
 
-    #[Route('/search', name: 'search')]
-    public function index(Request $request): Response
+    /**
+     * Creates a new ActionItem entity.
+     */
+    #[Route('/search', name: 'ajax_search',methods: ['GET'])]
+    public function searchAction(Request $request)
     {
+        $requestString = $request->get('q');
 
+        $entities =  $this->patientService->findByEGN($requestString);
 
-        $requestString = $request->query->get('q');
-
-        $entities = $this->patientService->findByEGN($requestString);
-
-        if (!$entities) {
-            $result['entities']['error'] = "keine Einträge gefunden";
+        if(!$entities) {
+            $result['entities']['error'] = "Не е открит резултат!";
         } else {
             $result['entities'] = $this->getRealEntities($entities);
         }
@@ -35,14 +39,17 @@ class SearchController extends AbstractController
         return new Response(json_encode($result));
     }
 
-    public function getRealEntities($entities)
+    /**
+     * @param $entities
+     * @return array
+     */
+    public function getRealEntities($entities): array
     {
 
-        foreach ($entities as $entity) {
-            $realEntities[$entity->getId()] = $entity->getFirstName();
+        foreach ($entities as $entity){
+            $realEntities[$entity->getId()] = $entity->getFirstName()." ".$entity->getLastName()." ".$entity->getEGN()." ".$entity->getPatientID();
         }
 
         return $realEntities;
     }
-
 }
